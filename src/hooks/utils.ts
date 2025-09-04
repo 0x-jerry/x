024364519789b70f46ece15aab56e885e9ca379d
Jerry <x.jerry.wang@gmail.com>
2025-09-04 09:30:52 +0000

    chore: clean code

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -18,12 +18,10 @@ export function chainHooks<T extends LoadHook | ResolveHook>(fns: T[]): T {
   const wrapperFn = (a: unknown, b: unknown, defaultHook: any) => {
     const fn = fns.reduceRight((prev, hook) => {
       return (a: any, b: any) => {
-        // console.log(a, b, hook)
         return hook(a, b, prev)
       }
     }, defaultHook)
 
-    // console.log(a, b, fn)
     return fn(a, b)
   }
 
