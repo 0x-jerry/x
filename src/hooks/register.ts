804251dff2b2509e3980ba8fc00612b200009cc2
Jerry <x.jerry.wang@gmail.com>
2025-09-08 06:45:37 +0000

    chore: add some explaination

--- src/hooks/register.ts
+++ src/hooks/register.ts
@@ -1,6 +1,8 @@
 import { register } from 'node:module'
 
+// tsx must be the first one, the order is matters.
 register('./tsx.js', import.meta.url)
+
 register('./text.js', import.meta.url)
 register('./jsonc.js', import.meta.url)
 register('./yaml.js', import.meta.url)
