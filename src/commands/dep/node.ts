f6fd3b6e8e5136549fddcfa362106e4314abf385
Jerry <x.jerry.wang@gmail.com>
2025-11-19 19:56:52 +0800

    fix: detect bun lock file

--- src/commands/dep/node.ts
+++ src/commands/dep/node.ts
@@ -131,7 +131,7 @@ export async function detectPkgManagerCommand(
     return 'pnpm'
   }
 
-  const bunLockFile = join(cwd, 'bun.lockb')
+  const bunLockFile = join(cwd, 'bun.lock')
   if (exists(bunLockFile)) {
     return 'bun'
   }
